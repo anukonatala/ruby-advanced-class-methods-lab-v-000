355ee26b5ad804e675c3dba5f2d826619cb1b8b6
Anusha Konatala <anu.konatala@gmail.com>
2018-06-18 18:16:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,52 @@ class Song
   def self.all
     @@all
   end
-
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+  song = self.new_by_name(name)
+  @@all << song
+  song
+  end
+  
+  def self.find_by_name(name)
+     self.all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+     found = self.find_by_name(name)
+     if(!found)
+       self.create_by_name(name)
+     else
+       found
+    end
+       
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+  
+  def self.new_from_filename
+    
+  end
   def save
     self.class.all << self
+  end
+  
+  def self.destroy_all
+    @@all.clear
   end
 
 end
