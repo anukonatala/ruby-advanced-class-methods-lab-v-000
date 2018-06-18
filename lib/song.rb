0671350fb3fecf6ce2d5bb28510eb7963723b3e3
Anusha Konatala <anu.konatala@gmail.com>
2018-06-18 18:18:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,8 @@ class Song
     self.all.sort_by{|song| song.name}
   end
   
-  def self.new_from_filename
+  def self.new_from_filename(filename)
+    filename.split("-")
     
   end
   def save
